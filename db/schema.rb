5735028be1ec2da15dbf2334cce9ad1b9c087c5e
Will Mavis <willard.mavis@gmail.com>
2018-07-14 18:07:20 -0500

    rails generate scaffold photo avatar:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_14_230603) do
+ActiveRecord::Schema.define(version: 2018_07_14_230711) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 2018_07_14_230603) do
 
   create_table "comments", force: :cascade do |t|
     t.string "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.string "avatar"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
